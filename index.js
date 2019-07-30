e4cdeeb7e5574cf026103c3bd1dad1cbf529128b
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-30 19:54:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
-    if(item === cart[i].itemName){
+    if(cart[i].itemName.item){
       cart.splice(item)
       return cart
     }

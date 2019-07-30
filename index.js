a87913bfa3314a5f500197b23c23da7b7795bf36
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-30 20:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,5 +60,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   var orderComplete = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  cart = []
   
 }
